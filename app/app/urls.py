bef5c6949435fb24668f4d147ff02dab7db6f760
Anton <kovalsckij.anton@yandex.ru>
2024-01-18 02:05:46 +0500

    html for page and images

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from main import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.index, name = 'index'),
@@ -26,3 +29,6 @@ urlpatterns = [
     path("last_vac/", views.last_vac, name = 'last_vac'),
     path("georgraph/", views.georgraph, name = 'georgraph'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/main/admin.py
+++ app/main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import VostrebContent
+from .models import Image
+@admin.register(VostrebContent)
+class VostrebContent(admin.ModelAdmin):
+    pass
 
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
 # Register your models here.
