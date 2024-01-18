cdbd04c1b571ccdcf44fc54fbedba5682fdc1da2
Anton <kovalsckij.anton@yandex.ru>
2024-01-18 21:20:17 +0500

    made all models

--- app/main/admin.py
+++ app/main/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
+from .models import SkillsContent
+from .models import GeorgraphContent
 from .models import VostrebContent
 from .models import Image
+
+@admin.register(SkillsContent)
+class SkillsContentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(GeorgraphContent)
+class GeorgraphContentAdmin(admin.ModelAdmin):
+    pass
 @admin.register(VostrebContent)
-class VostrebContent(admin.ModelAdmin):
+class VostrebContentAdmin(admin.ModelAdmin):
     pass
 
 @admin.register(Image)
--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from DataForSite.lastvacancieHHAPi import get_lact_vac
+from .models import SkillsContent
+from .models import GeorgraphContent
 from .models import VostrebContent
 def index(request):
     context = {
@@ -10,14 +12,18 @@ def index(request):
     #return HttpResponse("Home page")
 
 def vostreb(request):
+    result = VostrebContent.objects.all()
     context = {
         'title': "Vostreb",
+        'result': result
     }
     return render(request, 'main/vostreb.html', context)
     #return HttpResponse("Home page")
 def skills(request):
+    result = SkillsContent.objects.all()
     context = {
         'title': "skills",
+        'result': result
     }
     return render(request, 'main/skills.html', context)
     #return HttpResponse("Home page")
@@ -29,7 +35,7 @@ def last_vac(request):
     return render(request, 'main/last_vac.html', context)
     #return HttpResponse("Home page")
 def georgraph(request):
-    result = VostrebContent.objects.all()
+    result = GeorgraphContent.objects.all()
     context = {
         'title': "georgraph",
         'result': result
